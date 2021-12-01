5c05c0cada22ab15f2050b47206539a4bb064d6c
Ian Cannon <ian.cannon@protonmail.com>
2021-12-01 11:19:38 -0800

    Implement login functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,12 +11,7 @@ export default function NavBar(props) {
 
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-						&nbsp; | &nbsp;
       <Link to="" onClick={()=>handleLogOut()}>Log Out</Link>
-						&nbsp; | &nbsp;
 						<span>Welcome, {props.user.name}</span>
     </nav>
   );
