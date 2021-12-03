26d4366899acac001021ae57232e4db7a2511c2f
Ian Cannon <ian.cannon@protonmail.com>
2021-12-03 10:59:54 -0800

    Continue link building

--- src/components/SearchResultsItem/SearchResultsItem.jsx
+++ src/components/SearchResultsItem/SearchResultsItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SearchResultsItem({charity}){
 		return(
 			<div className="SearchResultsItem">
+					<Link to={`/charities/${charity.charityName}`}>
 					<h4 className="CharityName">{charity.charityName}</h4>
 					<h5>{charity.charityCause}</h5>
 					<h5>{charity.charityRating}</h5>
+					</Link>
 			</div>
 		);
 }
