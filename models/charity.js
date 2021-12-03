6923ccfc81a7b4404f8ed4e9deaedd64827c2185
Ian Cannon <ian.cannon@protonmail.com>
2021-12-03 15:29:33 -0800

    Finish MODEL for charity

--- models/charity.js
+++ models/charity.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const charitySchema = new Schema({
-
+		name: {type: String, require: true},
+		rating: {type: Number},
+		cause: {type: String},
+		mission: {type: String},
+		url: {type: String},
 });
 
 module.exports = mongoose.model('Charity', charitySchema);
