65e38b72286cd5d87a5a8bb53be8e2873416f452
Ian Cannon <ian.cannon@protonmail.com>
2022-04-06 14:16:44 -0700

    Update images

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ export default function NavBar({user, setUser}) {
 						<br/>
 					 <span>Welcome, {user.name}</span>&nbsp;◈&nbsp;
 						<Link to="/donations/new" className="button btm-sm">Make a Donation</Link>
-						<img className="Logo" alt="" src="https://i.imgur.com/q8e9ujq.png"/>
+						<img className="Logo" alt="" src="https://i.imgur.com/i9RbClm.png"/>
 			   <Link to="/donations" className="button btm-sm">See Your Giving</Link>&nbsp;◈&nbsp;
       <Link to="" onClick={()=> handleLogOut()}>Log Out</Link>
       <br/>
